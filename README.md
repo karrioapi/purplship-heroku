554379f2cf3145c8f941205c1229cf257431f6cf
Dan Kobina <danielkobina@gmail.com>
2020-10-09 18:24:55 -0400

    Setup Heroku app for purplship-server demo

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # purplship-server-demo
- Purplship server demo deployment
+
+This is a demo instance of [purplship-server](https://github.com/PurplShip/purplship-server)
+deployed on heroku.
+
+## Give it a whirl
+
+### Play with the Live Demo instance
+
+visit [purplship-demo](https://purplship-demo.herokuapp.com/)
+
+Demo Credentials | |
+--- | --- |
+username | `admin`  |
+password | `demo` |
+
+*Note that the demo server is public so usage data is wiped regularly*
+
+
+### Deploy your own instance on Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/PurplShip/purplship-server-demo/tree/main/)
+
+*Note that the plan is Hobby Dev and Free.*
--- app/settings.py
+++ app/settings.py
@@ -1,22 +1,20 @@
-from purpleserver.settings import *
+import os
+
+os.environ.setdefault('DATABASE_PASSWORD', '')
+os.environ.setdefault('DATABASE_USERNAME', '')
+os.environ.setdefault('DATABASE_HOST', '')
+os.environ.setdefault('DATABASE_PORT', '')
+
+from purpleserver.settings import *  # noqa
 
 # HTTPS configuration
-if USE_HTTPS is True:
-    global SECURE_SSL_REDIRECT
-    global SECURE_PROXY_SSL_HEADER
-    global SESSION_COOKIE_SECURE
-    global SECURE_HSTS_SECONDS
-    global SECURE_HSTS_INCLUDE_SUBDOMAINS
-    global CSRF_COOKIE_SECURE
-    global SECURE_HSTS_PRELOAD
-
-    SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SESSION_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 1
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 1
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_PRELOAD = True
 
 
 SECURITY, *EXTRA_MIDDLEWARE = MIDDLEWARE
@@ -26,11 +24,9 @@ MIDDLEWARE = (
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
-DATABASE_URL = config('DATABASE_URL', default=None, cast=str)
-if DATABASE_URL is None:
-    import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+import dj_database_url  # noqa
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Simplified static file serving.
