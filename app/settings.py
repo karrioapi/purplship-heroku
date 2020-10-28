0f366b6426d6f9fa527513babbae43cda386c379
Dan Kobina <danielkobina@gmail.com>
2020-10-28 07:24:41 -0400

    update dependencies to 2020.10 requirements

--- app/settings.py
+++ app/settings.py
@@ -1,20 +1,5 @@
-import os
-
-os.environ.setdefault('DATABASE_PASSWORD', '')
-os.environ.setdefault('DATABASE_USERNAME', '')
-os.environ.setdefault('DATABASE_HOST', '')
-os.environ.setdefault('DATABASE_PORT', '')
-
-from purpleserver.settings import *  # noqa
-
-# HTTPS configuration
-SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 1
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_PRELOAD = True
+import dj_database_url
+from purpleserver.settings import *
 
 
 SECURITY, *EXTRA_MIDDLEWARE = MIDDLEWARE
@@ -24,7 +9,6 @@ MIDDLEWARE = (
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url  # noqa
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
